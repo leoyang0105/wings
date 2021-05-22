6f10b6770152f7897860079aed49bb347809b851
Leo <leo@xian.dev>
2021-05-22 22:32:14 +0800

    Add part of DDD Application

---
+++ src/Wings.Application/Models/PagedResult.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Wings.Application.Models
+{
+    public class PagedResult<T> : IPagedResult<T> where T : class
+    {
+        public int PageIndex { get; private set; }
+        public int PageSize { get; private set; }
+        public int TotalCount { get; private set; }
+        public int TotalPages { get; private set; }
+        public IEnumerable<T> Items { get; private set; }
+
+        public PagedResult(IQueryable<T> source, int pageIndex, int pageSize)
+        {
+            if (pageIndex > 0)
+                pageIndex--;
+            int totalCount = source.Count();
+            TotalCount = totalCount;
+            TotalPages = totalCount / pageSize;
+
+            if (totalCount % pageSize > 0)
+                TotalPages++;
+            PageSize = pageSize;
+            PageIndex = pageIndex + 1;
+            Items = source.Skip(pageIndex * pageSize).Take(pageSize);
+        }
+
+        public PagedResult(IEnumerable<T> source, int pageIndex, int pageSize)
+        {
+            if (pageIndex > 0)
+                pageIndex--;
+            int totalCount = source.Count();
+            TotalCount = totalCount;
+            TotalPages = totalCount / pageSize;
+
+            if (totalCount % pageSize > 0)
+                TotalPages++;
+            PageSize = pageSize;
+            PageIndex = pageIndex + 1;
+            Items = source.Skip(pageIndex * pageSize).Take(pageSize);
+        }
+    }
+}
---
+++ src/Wings.Application/Models/SelectListItemModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Wings.Application.Models
+{
+    public class SelectListItemModel
+    {
+        public object Value { get; set; }
+        public string Text { get; set; }
+    }
+}
