8b10ba61ea67dc50c5baf30f449f04e9ab8c411c
Leo <leo@xian.dev>
2021-08-24 16:38:53 +0800

    Add EventBus

---
+++ src/Wings.EventBus/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Wings.EventBus
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddWingsEventBusInMemory(this IServiceCollection services, Assembly[] assemblies)
+        {
+            services.AddMassTransit(cfg =>
+            {
+                cfg.AddConsumers(assemblies);
+                cfg.UsingInMemory((ctx, c) =>
+                {
+                    c.TransportConcurrencyLimit = 100;
+                    c.ReceiveEndpoint(x =>
+                    {
+                        x.ConfigureConsumers(ctx);
+                    });
+                });
+            });
+            return services;
+        }
+    }
+}
---
+++ src/Wings.EventBus/IIntegrationEventConsumer.cs
@@ -0,0 +1,9 @@
+﻿using MassTransit;
+
+namespace Wings.EventBus
+{
+    public interface IIntegrationEventConsumer<in TIntegrationEvent> : IConsumer<TIntegrationEvent>
+         where TIntegrationEvent : IntegrationEvent
+    {
+    }
+}
