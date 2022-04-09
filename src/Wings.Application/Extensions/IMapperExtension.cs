7e58a7b9654e25729e14c2ed0ec5afbced0248a6
Leo <leoyang0105@gmail.com>
2022-04-09 17:57:23 +0800

    Add ProjectToPaged extension for AutoMapper

---
+++ src/Wings.Application/Extensions/IMapperExtension.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using System.Linq;
+using Wings.Application.Models;
+
+namespace Wings.Application.Extensions
+{
+    public static class IMapperExtension
+    {
+        public static IPagedResult<TDestination> ProjectToPaged<TDestination>(this IMapper mapper, IQueryable source, int pageIndex, int pageSize)
+            where TDestination : class
+        {
+            return new PagedResult<TDestination>(mapper.ProjectTo<TDestination>(source), pageIndex, pageSize);
+        }
+    }
+}
